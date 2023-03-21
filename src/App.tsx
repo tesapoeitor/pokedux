425aee842a4eae75fab18ba2f4c65999d9fd379c
Enrique Aguilera <enriqueaguilerapena@gmail.com>
2023-03-20 21:20:15 -0300

    reemplace connect con hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
 import { Searcher } from "./components/Searcher"
 import { PokemonList } from "./components/PokemonList"
 import { usePokeAPI } from "./hooks/usePokeAPI"
-import { connect } from "react-redux"
-import { setPokemons as setPokemonsAction } from "./actions"
-import axios from "axios"
-import { useEffect } from "react"
-import type { PokeAPI } from "./types/PokeAPI"
-import type { PokemonsState } from "./types/PokemonsState"
-import type { Dispatch } from "redux"
-import type { Action } from "./types/PokemonsAction"
 
-interface Props {
-  pokemons: PokeAPI["results"]
-  setPokemons: (value: PokeAPI["results"]) => Action
-}
-
-function App({ pokemons, setPokemons }: Props) {
-console.log("🚀 ~ file: App.tsx:19 ~ App ~ pokemons:", pokemons)
-
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=151"
-
-  const fetchData =async () => {
-      const response = await axios.get<PokeAPI>(API)
-      const data = response.data
-
-      setPokemons(data.results)
-  }
-
-  useEffect(() => {
-      fetchData()
-  }, [])
+function App() {
+  const pokemons = usePokeAPI()
 
   return (
     <div className="App">
@@ -39,12 +13,4 @@ console.log("🚀 ~ file: App.tsx:19 ~ App ~ pokemons:", pokemons)
   )
 }
 
-const mapStateToProps = (state: PokemonsState) => ({
-  pokemons: state.pokemons
-})
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  setPokemons: (value: PokeAPI["results"]) => dispatch(setPokemonsAction(value))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
--- src/hooks/usePokeAPI.ts
+++ src/hooks/usePokeAPI.ts
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import  axios from "axios"
+import { useSelector, useDispatch } from "react-redux"
+import { setPokemons } from "../actions"
 import type { PokeAPI } from "../types/PokeAPI"
+import type { PokemonsState } from "../types/PokemonsState"
 
 
 const usePokeAPI = () => {
-    const [pokemons, setPokemons] = useState<PokeAPI["results"]>()
+    const pokemons = useSelector((state: PokemonsState) => state.pokemons)
+    const dispatch = useDispatch()
+
     const API = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
     const fetchData =async () => {
         const response = await axios.get<PokeAPI>(API)
         const data = response.data
 
-        setPokemons(data.results)
+        dispatch(setPokemons(data.results))
     }
 
     useEffect(() => {
